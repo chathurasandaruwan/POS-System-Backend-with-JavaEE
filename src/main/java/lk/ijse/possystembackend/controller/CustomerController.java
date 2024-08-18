366a3a833f3e009c50b5320f4771aebcad46fd7e
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-08-18 15:30:23 +0530

    check post save customer using postman

--- src/main/java/lk/ijse/possystembackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/possystembackend/controller/CustomerController.java
@@ -1,10 +1,13 @@
 package lk.ijse.possystembackend.controller;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.possystembackend.dto.CustomerDTO;
 
 import java.io.IOException;
 
@@ -12,12 +15,13 @@ import java.io.IOException;
 public class CustomerController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       /* if (!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null) {
+        if (!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null) {
 //            send error
             resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
-        }*/
-        String name = req.getParameter("id");
-        System.out.println(name);
+        }
+        Jsonb jsonb = JsonbBuilder.create();
+        CustomerDTO customerDTO = jsonb.fromJson(req.getReader() , CustomerDTO.class);
+        System.out.println(customerDTO);
 
     }
 }
