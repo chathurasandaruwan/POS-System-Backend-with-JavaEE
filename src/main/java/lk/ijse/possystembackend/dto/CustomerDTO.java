09591cfe1d3d060770bb38106b53fd211012fc26
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-08-18 12:54:31 +0530

    create customerDto

--- src/main/java/lk/ijse/possystembackend/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/possystembackend/dto/CustomerDTO.java
@@ -1,4 +1,16 @@
 package lk.ijse.possystembackend.dto;
 
-public class CustomerDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomerDTO implements Serializable {
+    String customerId;
+    String customerName;
+    String customerAdd;
+    String customerSalary;
 }
