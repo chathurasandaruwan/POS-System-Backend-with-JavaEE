b51085fed9f88c1fee1722d2745770c08fc10cee
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-08-18 13:38:40 +0530

    check save customer using postman

--- src/main/java/lk/ijse/possystembackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/possystembackend/controller/CustomerController.java
@@ -1,4 +1,23 @@
 package lk.ijse.possystembackend.controller;
 
-public class CustomerController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       /* if (!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null) {
+//            send error
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }*/
+        String name = req.getParameter("id");
+        System.out.println(name);
+
+    }
 }
