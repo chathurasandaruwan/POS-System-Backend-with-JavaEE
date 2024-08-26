f10a8a7643f81debde04d0dd2abaec24fdaaa522
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-08-26 19:17:42 +0530

    create filter layer

--- src/main/java/lk/ijse/possystembackend/entity/PlaceOrder.java
+++ src/main/java/lk/ijse/possystembackend/entity/PlaceOrder.java
@@ -2,6 +2,8 @@ package lk.ijse.possystembackend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +20,5 @@ public class PlaceOrder {
     String price;
     String qty;
     String total;
+
 }
